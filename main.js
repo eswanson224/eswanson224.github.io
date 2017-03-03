e0792a80eb040f701a30aeea2ee42be5d501dd32
ErikNathan <erik.a.swanson9@gmail.com>
2017-03-03 12:28:26 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
   var poptarts = 0;
   var poptartsd = poptarts + ' Poptarts';
   var toasters = 0;
+  var farms = 0;
   var pps = 0;
   var ppsd = pps + ' Poptarts Per Second';
-  var toasterprice = 100;
   var toasterpricer = 100;
   var toasterpriced = '';
+  var farmpricer = 10000;
+  var farmpriced = '';
   function Click() {
       poptarts += 1;
       if (poptarts == 1) {
@@ -36,14 +38,22 @@
       setTimeout(Pps, 1000);
 }
   function Toaster() {
-      if (poptarts >= toasterprice) {
+      if (poptarts >= toasterpricer) {
       toasters += 1;
       poptarts -= toasterpricer;
-      toasterprice += toasterprice*(0.05*toasters);
-      toasterpricer = Math.round(toasterprice)
+      toasterpricer = Math.round(toasterpricer*(0.05*toasters));
       toasterpriced = toasterpricer + ' Poptarts';
       document.getElementById("toasters").innerHTML = toasters;
       document.getElementById("poptarts").innerHTML = poptartsd;
       document.getElementById("toasterprice").innerHTML = toasterpriced;
 }
-}
+    function Farm() {
+      if (poptarts >= farmpricer) {
+      farms += 1;
+      poptarts -= farmpricer;
+      farmpricer = Math.round(farmpricer*(0.05*farms));
+      toasterpriced = toasterpricer + ' Poptarts';
+      document.getElementById("farms").innerHTML = farms;
+      document.getElementById("poptarts").innerHTML = poptartsd;
+      document.getElementById("farmprice").innerHTML = farmpricer;
+      }
