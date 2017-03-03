271e9c32b2e03ca3030bc745cecc1ca5b8e13da8
Krazee50 <erik.a.swanson9@gmail.com>
2017-03-02 20:29:23 -0600

    F

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
   var poptartsd = poptarts + ' Poptarts';
   var toasters = 0;
   var pps = 0;
-  var ppsd = pps + ' Poptarts Per Second'
+  var ppsd = pps + ' Poptarts Per Second';
+  var toasterprice = 100;
+  var toasterpricer = 100;
+  var toasterpriced = '';
   function Click() {
       poptarts += 1;
       if (poptarts == 1) {
@@ -33,10 +36,14 @@
       setTimeout(Pps, 1000);
 }
   function Toaster() {
-      if (poptarts >= 100) {
+      if (poptarts >= toasterprice) {
       toasters += 1;
-      poptarts -= 100;
+      poptarts -= toasterpricer;
+      toasterprice += toasterprice*(0.05*toasters);
+      toasterpricer = Math.round(toasterprice)
+      toasterpriced = toasterpricer + ' Poptarts';
       document.getElementById("toasters").innerHTML = toasters;
       document.getElementById("poptarts").innerHTML = poptartsd;
+      document.getElementById("toasterprice").innerHTML = toasterpriced;
 }
 }
