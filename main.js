84cfcd39b7458aa83baa59aa966d641a19103896
ErikNathan <erik.a.swanson9@gmail.com>
2017-03-07 19:33:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,81 +1,107 @@
-  var poptarts = 0;
-  var poptartsd = poptarts + ' Poptarts';
-  var toasters = 0;
-  var microwaves = 0;
-  var pps = 0;
-  var ppsd = pps + ' Poptarts Per Second';
-  var poptartss = '';
-  var toasterss = '';
-  var microwavess = '';
-  var toasterpricer = Math.round(100*Math.pow(1.05, toasters));
-  var toasterpriced = '';
-  var microwavepricer = Math.round(1000*Math.pow(1.05, microwaves));
-  var microwavepriced = '';
-  function Click() {
-      poptarts += 1;
-      if (poptarts == 1) {
-          poptartsd = poptarts + ' Poptart';
-      }
-      else {
-          poptartsd = poptarts + ' Poptarts';
-      }
-      document.getElementById("poptarts").innerHTML = poptartsd;
-}
-  function Pps() {
-      pps = toasters+15*microwaves;
-      poptarts += pps;
-      if (poptarts == 1) {
-          poptartsd = poptarts + ' Poptart';
-      }
-      else {
-          poptartsd = poptarts + ' Poptarts';
-      }
-      document.getElementById("poptarts").innerHTML = poptartsd;
-       if (pps == 1) {
-          ppsd = pps + ' Poptart Per Second';
-      }
-      else {
-          ppsd = pps + ' Poptarts Per Second';
-      }
-      document.getElementById("pps").innerHTML = ppsd;
-      setTimeout(Pps, 1000);
-}
-  function Save() {
-    localStorage.setItem("poptarts", poptarts);
-    localStorage.setItem("toasters", toasters);
-    localStorage.setItem("microwaves", microwaves);
-}
-  function Load() {
-    poptartss = localStorage.getItem("poptarts");
-    toasterss = localStorage.getItem("toasters");
-    microwavess = localStorage.getItem("microwaves");
-    poptarts = Number(poptartss);
-    toasters = Number(toasterss);
-    microwaves = Number(microwavess);
-    Pps();
-    document.getElementById("toasters").innerHTML = toasters;
-    document.getElementById("microwaves").innerHTML = microwaves;
-    setTimeout(Save, 30000);
-}
-  function Toaster() {
-      if (poptarts >= toasterpricer) {
-      toasters += 1;
-      poptarts -= toasterpricer;
-      toasterpricer += Math.round(100*Math.pow(1.05, toasters));
-      toasterpriced = toasterpricer + ' Poptarts';
-      document.getElementById("toasters").innerHTML = toasters;
-      document.getElementById("poptarts").innerHTML = poptartsd;
-      document.getElementById("toasterprice").innerHTML = toasterpriced;
-      }
+var poptarts = 0;
+var poptartsd = poptarts + ' Poptarts';
+var toasters = 0;
+var toastersd = toasters + ' Toasters';
+var microwaves = 0;
+var microwavesd = microwaves + ' Microwaves';
+var toasterpricer = Math.round(100*Math.pow(1.05, toasters));
+var toasterpriced = toasterpricer + ' Poptarts';
+var microwavepricer = Math.round(1000*Math.pow(1.05, microwaves));
+var microwavepriced = microwavepricer + ' Poptarts';
+var pps = 0;
+var ppsd = pps + ' Poptarts Per Second';
+var poptartss = '';
+var toasterss = '';
+var microwavess = '';
+function Click() {
+  poptarts += 1;
+  if (poptarts == 1) {
+    poptartsd = poptarts + ' Poptart';
   }
-    function Microwave() {
-      if (poptarts >= microwavepricer) {
-      microwaves += 1;
-      poptarts -= microwavepricer;
-      microwavepricer += Math.round(1000*Math.pow(1.05, microwaves));
-      microwavepriced = microwavepricer + ' Poptarts';
-      document.getElementById("microwaves").innerHTML = microwaves;
-      document.getElementById("poptarts").innerHTML = poptartsd;
-      document.getElementById("microwaveprice").innerHTML = microwavepriced;
-      }
+  else {
+    poptartsd = poptarts + ' Poptarts';
+  }
+  document.getElementById("poptarts").innerHTML = poptartsd;
+}
+function Pps() {
+  pps = toasters+15*microwaves;
+  poptarts += pps;
+  if (poptarts == 1) {
+    poptartsd = poptarts + ' Poptart';
+  }
+  else {
+    poptartsd = poptarts + ' Poptarts';
+  }
+  document.getElementById("poptarts").innerHTML = poptartsd;
+  if (pps == 1) {
+    ppsd = pps + ' Poptart Per Second';
+  }
+  else {
+    ppsd = pps + ' Poptarts Per Second';
+  }
+  document.getElementById("pps").innerHTML = ppsd;
+  setTimeout(Pps, 1000);
+}
+function Save() {
+  localStorage.setItem("poptarts", poptarts);
+  localStorage.setItem("toasters", toasters);
+  localStorage.setItem("microwaves", microwaves);
+}
+function Load() {
+  poptartss = localStorage.getItem("poptarts");
+  toasterss = localStorage.getItem("toasters");
+  microwavess = localStorage.getItem("microwaves");
+  poptarts = Number(poptartss);
+  toasters = Number(toasterss);
+  microwaves = Number(microwavess);
+  Pps();
+  document.getElementById("toasters").innerHTML = toasters;
+  document.getElementById("microwaves").innerHTML = microwaves;
+  setTimeout(Save, 30000);
+}
+function Toaster() {
+  if (poptarts >= toasterpricer) {
+    toasters += 1;
+    poptarts -= toasterpricer;
+    toasterpricer += Math.round(100*Math.pow(1.05, toasters));
+    toasterpriced = toasterpricer + ' Poptarts';
+    if (poptarts == 1) {
+      poptartsd = poptarts + ' Poptart';
     }
+    else {
+      poptartsd = poptarts + ' Poptarts';
+    }
+    if (toasters == 1) {
+      toastersd = toasters + ' Toaster';
+    }
+    else {
+      toastersd = toasters + ' Toasters';
+    }
+    document.getElementById("toasters").innerHTML = toastersd;
+    document.getElementById("poptarts").innerHTML = poptartsd;
+    document.getElementById("toasterprice").innerHTML = toasterpriced;
+  }
+}
+function Microwave() {
+  if (poptarts >= microwavepricer) {
+    microwaves += 1;
+    poptarts -= microwavepricer;
+    microwavepricer += Math.round(1000*Math.pow(1.05, microwaves));
+    microwavepriced = microwavepricer + ' Poptarts';
+    if (poptarts == 1) {
+      poptartsd = poptarts + ' Poptart';
+    }
+    else {
+      poptartsd = poptarts + ' Poptarts';
+    }
+    if (microwaves == 1) {
+      microwavesd = microwaves + ' Microwave';
+    }
+    else {
+      microwavesd = microwaves + ' Microwaves';
+    }
+    document.getElementById("microwaves").innerHTML = microwavesd;
+    document.getElementById("poptarts").innerHTML = poptartsd;
+    document.getElementById("microwaveprice").innerHTML = microwavepriced;
+  }
+}
